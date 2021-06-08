b9b54cd0224550cf075a34fcf8b9b823281c5531
lucasmassias <lucasmassiasfreitas@gmail.com>
2021-06-08 18:14:25 -0300

    Dia 5.2 Exercicio pt1, Buscando Elementos

---
+++ exercicios/5.2/buscandoElementos.js
@@ -0,0 +1,7 @@
+document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = "red";
+document.getElementById("primeiroFilhoDoFilho").innerText = "Leonardo";
+document.querySelector("#Pai").firstElementChild;
+document.querySelector("#elementoOndeVoceEsta").previousElementSibling;
+document.querySelector("#elementoOndeVoceEsta").nextSibling;
+document.querySelector("#elementoOndeVoceEsta").nextElementSibling;
+document.querySelector("#pai").parentNode.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling;
