c143e6528ffefb2929e689bdb1606cb31bb2e982
lucasmassias <lucasmassiasfreitas@gmail.com>
2021-06-08 12:47:52 -0300

    dia 5.1 concluido, commitao

---
+++ exercicios/5.1/pt2 Seletores de Elementos/script.js
@@ -0,0 +1,7 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+document.getElementById("page-title").innerText = "Filmes Favoritos!";
+document.getElementById("second-paragraph").innerText = "Descrição para o subtitulo!";
+document.getElementById("subtitle").innerText = "Os porquês da Vida";
+document.getElementsByClassName("claspara")[0].style.color = "Blue";
+document.getElementsByTagName("h4")[0].style.color = "Green";
