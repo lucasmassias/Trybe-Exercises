2369607df047fd508da6fcbea37878e3185aa1bc
lucasmassias <lucasmassiasfreitas@gmail.com>
2021-09-03 19:31:47 -0300

    2 do para fixar do 8.2 array.find()

---
+++ exercicios/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/8.2 - Para Fixar - array.find().js
@@ -0,0 +1,18 @@
+// 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  // Adiciona seu código aqui
+  return numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+}
+console.log(findDivisibleBy3And5(numbers))
+
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  // Adicione seu código aqui:
+  return names.find((name) => name.length === 5);
+}
+
+console.log(findNameWithFiveLetters(names));
